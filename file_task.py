432ba96d59f18624020b2c6755b60993cd61b536
Lana Novikova <svetlnovikova@gmail.com>
2021-09-15 22:05:47 +0300

    removed cycle

--- file_task.py
+++ file_task.py
@@ -11,5 +11,4 @@ with open('referat.txt','r',encoding='utf-8') as ref:
         word_count = word_count+1
 print(word_count)
 with open('referat2.txt','w',encoding='utf8') as ref2:
-    for line in ref:
-        ref2.writelines(line)
+    ref2.write(ref)
